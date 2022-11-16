dc958a2a5d9d431df9a4acfec062d83a2b9dc4be
ivan <ivanlatorre720@gmail.com>
2022-11-16 15:45:15 -0300

    poo

---
+++ practicas/src/POO/Uso_Coche.java
@@ -0,0 +1,16 @@
+package POO;
+
+public class Uso_Coche {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		coche Renault=new coche(); // instanciar una clases, ejemplar de clase
+		
+		
+		System.out.println("Este coche tiene "+Renault.ruedas);
+		
+		
+	}
+
+}
---
+++ practicas/src/POO/coche.java
@@ -0,0 +1,32 @@
+package POO;
+
+public class coche {
+
+	int ruedas;
+	
+	int largo;
+	
+	int ancho;
+	
+	int motor;
+	
+	int peso;
+	
+	// metodo constructor
+	// da un estado inicial
+	
+	public coche() {
+		
+		ruedas=4;
+		
+		largo=2000;
+		
+		ancho=1600;
+		
+		peso=500;
+		
+		
+	}
+	
+
+}
